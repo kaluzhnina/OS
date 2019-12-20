1c5cbc0c297ed4a283322942f122ca5e6e203dab
kaluta <kaluzhninatatyana@gmail.com>
2019-12-20 11:31:19 +0300

    replacing an if condition with a ternary operator

--- lab_2/2_lab.c
+++ lab_2/2_lab.c
@@ -31,26 +31,16 @@ int main() {
             entry = namelist[i];
             stat (entry->d_name,buffer);
 
-            if (buffer->st_mode & S_IFDIR) printf("d");
-            else printf("-");
-            if (buffer->st_mode & S_IRUSR) printf("r");
-            else printf("-");
-            if (buffer->st_mode & S_IWUSR) printf("w");
-            else printf("-");
-            if (buffer->st_mode & S_IXUSR) printf("x");
-            else printf("-");
-            if (buffer->st_mode & S_IRGRP) printf("r");
-            else printf("-");
-            if (buffer->st_mode & S_IWGRP) printf("w");
-            else printf("-");
-            if (buffer->st_mode & S_IXGRP) printf("x");
-            else printf("-");
-            if (buffer->st_mode & S_IROTH) printf("r");
-            else printf("-");
-            if (buffer->st_mode & S_IWOTH) printf("w");
-            else printf("-");
-            if (buffer->st_mode & S_IXOTH) printf("x");
-            else printf("-");
+            printf((buffer->st_mode & S_IFDIR)?"d":"-");
+            printf((buffer->st_mode & S_IRUSR)?"r":"-");
+            printf((buffer->st_mode & S_IWUSR)?"w":"-");
+            printf((buffer->st_mode & S_IXUSR)?"x":"-");
+            printf((buffer->st_mode & S_IRGRP)?"r":"-");
+            printf((buffer->st_mode & S_IWGRP)?"w":"-");
+            printf((buffer->st_mode & S_IXGRP)?"x":"-");
+            printf((buffer->st_mode & S_IROTH)?"r":"-");
+            printf((buffer->st_mode & S_IWOTH)?"w":"-");
+            printf((buffer->st_mode & S_IXOTH)?"x":"-");
             
             printf("\t%5lld", buffer->st_size );
 
