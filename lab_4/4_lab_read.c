68214764ce58983f118502a48b0bdb1c30215252
kaluta <kaluzhninatatyana@gmail.com>
2019-12-26 20:51:31 +0300

    lab_4 (shared memory)

---
+++ lab_4/4_lab_read.c
@@ -0,0 +1,45 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h> 
+#include <string.h>
+
+#define SHMSZ 32
+#define FILENAME "fileshm"
+
+  
+int main() 
+{ 
+    time_t real_time;
+    time_t tm; 
+    int shmid;
+
+    char *shm;
+    key_t key = ftok(FILENAME, 1);
+    if ((shmid = shmget(key, SHMSZ, 0666)) < 0) 
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) 
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1)
+    {
+        real_time = time(NULL);
+        sleep(1);
+        printf("PID -- %d;\nTime now: %s\n", getpid(), ctime(&real_time));
+    
+        printf("Data read from memory: %s\n",shm); 
+    }
+
+   shmdt(shm);
+     
+    return 0; 
+} 
---
+++ lab_4/4_lab_write.c
@@ -0,0 +1,43 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h> 
+#include <string.h>
+
+#define SHMSZ 32
+#define FILENAME "fileshm"
+
+int main() 
+{ 
+    time_t tm; 
+    int shmid;
+    char* shm, *s;
+
+    key_t key = ftok(FILENAME, 1);
+    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) 
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) 
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        tm = time(NULL);
+        pid_t pid = getpid();
+        sprintf(s, "%d", (int)pid);
+        sprintf(shm, "%s%s%s", ctime(&tm), "PID -- ", s);
+        sleep(1);
+    }
+    shmdt(shm);
+    shmctl(shmid,IPC_RMID,NULL); 
+    return 0;
+}
+  
+  
