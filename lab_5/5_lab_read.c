3109ca8ea8f2c547e6828842e80b77bd1d8bf0b5
kaluta <kaluzhninatatyana@gmail.com>
2019-12-26 23:31:54 +0300

    lab_5 (shared memory whith semaphore)

---
+++ lab_5/5_lab_read.c
@@ -0,0 +1,57 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h> 
+#include <string.h>
+#include <sys/sem.h>
+
+#define SHMSZ 32
+#define FILENAME "fileshm"
+
+  
+int main() 
+{ 
+    time_t real_time;
+    time_t tm; 
+    int shmid;
+    int semid;
+    struct sembuf sem_lock = { 0, -1, 0};
+    struct sembuf sem_unlock = { 0, 1, 0};
+
+
+    char *shm;
+    key_t key = ftok(FILENAME, 1);
+    if((semid = semget(key, 1, 0666)) < 0)
+    {
+        perror("semid:can not get semaphore");
+        exit(EXIT_FAILURE);
+    }
+    if ((shmid = shmget(key, SHMSZ, 0666)) < 0) 
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) 
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1)
+    {
+        semop(semid, &sem_lock, 1);
+        real_time = time(NULL);
+        sleep(1);
+        printf("PID -- %d;\nTime now: %s\n", getpid(), ctime(&real_time));
+    
+        printf("Data read from memory: %s\n",shm); 
+        semop(semid, &sem_unlock, 1);
+    }
+
+   shmdt(shm);
+     
+    return 0; 
+} 
---
+++ lab_5/5_lab_write.c
@@ -0,0 +1,55 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h> 
+#include <string.h>
+#include <sys/sem.h>
+
+#define SHMSZ 32
+#define FILENAME "fileshm"
+
+int main() 
+{ 
+    time_t tm; 
+    int shmid;
+    int semid;
+    char* shm, *s;
+    struct sembuf sem_lock = { 0, -1, 0};
+    struct sembuf sem_unlock = { 0, 1, 0};
+
+    key_t key = ftok(FILENAME, 1);
+    if((semid = semget(key, 1, IPC_CREAT | 0666)) < 0)
+    {
+        perror("semid:can not get semaphore");
+        exit(EXIT_FAILURE);
+    }
+    if ((shmid = shmget(key, SHMSZ, IPC_CREAT | 0666)) < 0) 
+    {
+        perror("shmget");
+        exit(EXIT_FAILURE);
+    }
+    if ((shm = shmat(shmid, NULL, 0)) == (char *) -1) 
+    {
+        perror("shmat");
+        exit(EXIT_FAILURE);
+    }
+    semop(semid, &sem_unlock, 1);
+    while(1)
+    {
+        semop(semid, &sem_lock, 1);
+        tm = time(NULL);
+        pid_t pid = getpid();
+        sprintf(s, "%d", (int)pid);
+        sprintf(shm, "%s%s%s", ctime(&tm), "PID -- ", s);
+        sleep(1);
+        semop(semid, &sem_unlock, 1);
+    }
+    shmdt(shm);
+    shmctl(shmid,IPC_RMID,NULL); 
+    return 0;
+}
+  
+  
